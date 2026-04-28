72dc7ae3f60f78422dad57b200656a5647c3e4bf
Uehara Takuma <e245743@ie.u-ryukyu.ac.jp>
2026-04-28 09:34:21 +0900

    fit関数を実装

--- week3/regression.py
+++ week3/regression.py
@@ -6,10 +6,27 @@ class LinearRegression:
     y = None
     
     def fit(self, x, y):
-        pass
+      """
+      >>> import regression
+      >>> import datasets
+      >>> X, Y = datasets.load_linear_example1()
+      >>> import importlib
+      >>> model = regression.LinearRegression()
+      >>> model.fit(X,Y)
+      >>> model.theta
+      array([5.30412371, 0.49484536])
+      """
+      temp = np.linalg.inv(np.dot(x.T, x))
+      self.theta = np.dot(np.dot(temp,x.T),y)
       
     def predict(self, x):
         pass
       
     def score(self, x, y):
         pass
+      
+      
+      
+if __name__ == "__main__":
+  import doctest
+  doctest.testmod()
